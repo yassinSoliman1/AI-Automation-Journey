2a6533d2b33310db6be0c4dd44158824629481c7
Yassin Hany <yassinhanyshawky@gmail.com>
2026-07-17 00:24:29 +0300

    Add Guess Game project

---
+++ guessGame.py
@@ -0,0 +1,15 @@
+print("hello ento Guess game guess the secret number to win choose from 1 to 10")
+secret_number = 9
+number_of_trys = 3
+guess_count = 0
+while guess_count < number_of_trys :
+    guess_count+=1
+    user_number=int(input("Guess: "))
+    if user_number == secret_number :
+        print(" congratulation thats the eight Number ") 
+        break
+else:
+    print('''
+Game over
+          fuck you hahahaha
+''')
